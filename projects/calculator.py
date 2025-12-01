2c124d38c2bcaae18c069304cddc97d70dcf4139
Abdulloh <anuraliyev71@gmail.com>
2025-12-01 19:42:03 +0500

    Calculator

--- projects/calculator.py
+++
@@ -1,8 +0,0 @@
-class Calculator:
-    def add(self, a, b):
-        return a + b
-
-    def divide(self, a, b):
-        if b == 0:
-            raise ZeroDivisionError("Cannot divide by zero")
-        return a / b
